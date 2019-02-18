8d30adf0120f2764d76a0ce79d8b55ddd4d7ccaf
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2019-02-17 17:09:46 -0800

    Set up the basic index.js as a google cloud function

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const fetch = require('node-fetch')
 const config = require('./config')
 
+const opts = {
+    level: 'all',
+    timestampFormat:'YYYY-MM-DD HH:mm:ss.SSS'
+  }
+const log = require('simple-node-logger').createSimpleLogger(opts)
+
 const fetch511Json = async () => {
     const response = await fetch(`http://api.511.org/Traffic/Events/?api_key=${config.apiKey}&format=json`)
-    return response.text()
+    const text = await response.text()
+    const trimmedText = text.trim()
+    return JSON.parse(trimmedText)
 }
 
-fetch511Json()
-    .then((responseBody) => {
-        console.log(responseBody)
+const parse511events = (responseJson) => {
+    responseJson.events.forEach(event => {
+        console.log('event is:')
+        console.log(event)
     })
+    return responseJson
+}
+
+exports.helloHttp = (req, res) => {
+    switch (req.method) {
+        case 'GET':
+            break
+        default:
+            res.sendStatus(405)
+            log.error(`${req.method} is not allowed as a method`)
+            return
+    }
+
+    fetch511Json()
+        .then((responseBody) => {
+            const parsedBody = parse511events(responseBody)
+            res.json(parsedBody)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
