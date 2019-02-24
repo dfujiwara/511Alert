750e8ba2378d083909c3c15f9c0897f932c2269d
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2019-02-24 11:04:05 -0800

    Initial eslint fixes

--- index.js
+++ index.js
@@ -2,63 +2,63 @@ const fetch = require('node-fetch')
 const config = require('./config')
 
 const opts = {
-    level: 'all',
-    timestampFormat:'YYYY-MM-DD HH:mm:ss.SSS'
-  }
+  level: 'all',
+  timestampFormat: 'YYYY-MM-DD HH:mm:ss.SSS'
+}
 const log = require('simple-node-logger').createSimpleLogger(opts)
 
 const fetch511Json = async () => {
-    //  Currently we don't support pagination which is why we increase the limit to 50.
-    const response = await fetch(`http://api.511.org/Traffic/Events/?api_key=${config.apiKey}&format=json&status=ACTIVE&in_effect_on=now&limit=50`)
-    const text = await response.text()
-    const trimmedText = text.trim()
-    return JSON.parse(trimmedText)
+  //  Currently we don't support pagination which is why we increase the limit to 50.
+  const response = await fetch(`http://api.511.org/Traffic/Events/?api_key=${config.apiKey}&format=json&status=ACTIVE&in_effect_on=now&limit=50`)
+  const text = await response.text()
+  const trimmedText = text.trim()
+  return JSON.parse(trimmedText)
 }
 
 const parse511events = (responseJson) => {
-    const filteredEvents = responseJson.events
-        .map(event => {
-            return event.headline
-        })
-        .filter(headline => {
-            return (headline.indexOf('San Francisco') != -1)
-        })
-    if (filteredEvents.length == 0) {
-        return 'No relevant traffic events'
-    }
-    return filteredEvents.reduce((headlineString, headline) => {
-        return `${headlineString}\n${headline}`
+  const filteredEvents = responseJson.events
+    .map(event => {
+      return event.headline
     })
+    .filter(headline => {
+      return (headline.indexOf('San Francisco') != -1)
+    })
+  if (filteredEvents.length == 0) {
+    return 'No relevant traffic events'
+  }
+  return filteredEvents.reduce((headlineString, headline) => {
+    return `${headlineString}\n${headline}`
+  })
 }
 
 const notifyIfttt = (eventSummary) => {
-    return fetch(config.iftttWebHook, {
-        method: 'post',
-        body: JSON.stringify({ 'value1': eventSummary}),
-        headers: { 'Content-Type': 'application/json' },
-    })
+  return fetch(config.iftttWebHook, {
+    method: 'post',
+    body: JSON.stringify({ 'value1': eventSummary }),
+    headers: { 'Content-Type': 'application/json' }
+  })
 }
 
 exports.trigger511 = (req, res) => {
-    switch (req.method) {
-        case 'GET':
-            break
-        default:
-            log.error(`${req.method} is not allowed as a method`)
-            res.sendStatus(405)
-            return
-    }
+  switch (req.method) {
+    case 'GET':
+      break
+    default:
+      log.error(`${req.method} is not allowed as a method`)
+      res.sendStatus(405)
+      return
+  }
 
-    fetch511Json()
-        .then((responseBody) => {
-            const eventSummary = parse511events(responseBody)
-            return notifyIfttt(eventSummary)
-        })
-        .then(() => {
-            res.sendStatus(200)
-        })
-        .catch((error) => {
-            log.error(`Encountered an error: ${error}`)
-            res.sendStatus(500)
-        })
+  fetch511Json()
+    .then((responseBody) => {
+      const eventSummary = parse511events(responseBody)
+      return notifyIfttt(eventSummary)
+    })
+    .then(() => {
+      res.sendStatus(200)
+    })
+    .catch((error) => {
+      log.error(`Encountered an error: ${error}`)
+      res.sendStatus(500)
+    })
 }
--- index.test.js
+++ index.test.js
@@ -3,89 +3,89 @@ jest.mock('node-fetch')
 const fetch = require('node-fetch')
 
 describe('testing', () => {
-    beforeEach(() => {
-        jest.useFakeTimers()
+  beforeEach(() => {
+    jest.useFakeTimers()
+  })
+  afterEach(() => {
+    jest.runAllTicks()
+    fetch.mockClear()
+  })
+  test('GET method with single traffic notice', (done) => {
+    fetch.mockImplementationOnce(() => {
+      const result = {
+        events: [
+          { headline: 'San Francisco traffic' },
+          { headline: 'Oakland traffic' }
+        ]
+      }
+      return { text: () => JSON.stringify(result) }
     })
-    afterEach(() => {
-        jest.runAllTicks()
-        fetch.mockClear()
+    trigger511(
+      { method: 'GET' },
+      {
+        sendStatus: (status) => {
+          expect(fetch.mock.calls.length).toBe(2)
+          const { body } = fetch.mock.calls[1][1]
+          const expectedBody = JSON.stringify({ 'value1': 'San Francisco traffic' })
+          expect(body).toBe(expectedBody)
+          expect(status).toBe(200)
+          done()
+        }
+      })
+  })
+  test('GET method with multiple traffic notices', (done) => {
+    fetch.mockImplementationOnce(() => {
+      const result = {
+        events: [
+          { headline: 'San Francisco traffic' },
+          { headline: 'Oakland traffic' },
+          { headline: 'Another San Francisco traffic' }
+        ]
+      }
+      return { text: () => JSON.stringify(result) }
     })
-    test('GET method with single traffic notice', (done) => {
-        fetch.mockImplementationOnce(() => {
-            const result = {
-                events: [
-                    { headline: 'San Francisco traffic' },
-                    { headline: 'Oakland traffic' }
-                ]
-            }
-            return {text: () => JSON.stringify(result)}
-        })
-        trigger511(
-            { method: 'GET' },
-            {
-                sendStatus: (status) => {
-                    expect(fetch.mock.calls.length).toBe(2)
-                    const {body} = fetch.mock.calls[1][1]
-                    const expectedBody = JSON.stringify({'value1': 'San Francisco traffic'})
-                    expect(body).toBe(expectedBody)
-                    expect(status).toBe(200)
-                    done()
-                }
-            })
+    trigger511(
+      { method: 'GET' },
+      {
+        sendStatus: (status) => {
+          const { body } = fetch.mock.calls[1][1]
+          const expectedBody = JSON.stringify({ 'value1': 'San Francisco traffic\nAnother San Francisco traffic' })
+          expect(body).toBe(expectedBody)
+          expect(status).toBe(200)
+          done()
+        }
+      })
+  })
+  test('GET method with no traffic notices', (done) => {
+    fetch.mockImplementationOnce(() => {
+      const result = {
+        events: [
+          { headline: 'Oakland traffic' }
+        ]
+      }
+      return { text: () => JSON.stringify(result) }
     })
-    test('GET method with multiple traffic notices', (done) => {
-        fetch.mockImplementationOnce(() => {
-            const result = {
-                events: [
-                    { headline: 'San Francisco traffic' },
-                    { headline: 'Oakland traffic' },
-                    { headline: 'Another San Francisco traffic' }
-                ]
-            }
-            return {text: () => JSON.stringify(result)}
-        })
-        trigger511(
-            { method: 'GET' },
-            {
-                sendStatus: (status) => {
-                    const {body} = fetch.mock.calls[1][1]
-                    const expectedBody = JSON.stringify({'value1': 'San Francisco traffic\nAnother San Francisco traffic'})
-                    expect(body).toBe(expectedBody)
-                    expect(status).toBe(200)
-                    done()
-                }
-            })
-    })
-    test('GET method with no traffic notices', (done) => {
-        fetch.mockImplementationOnce(() => {
-            const result = {
-                events: [
-                    { headline: 'Oakland traffic' },
-                ]
-            }
-            return {text: () => JSON.stringify(result)}
-        })
-        trigger511(
-            { method: 'GET' },
-            {
-                sendStatus: (status) => {
-                    expect(status).toBe(200)
-                    expect(fetch.mock.calls.length).toBe(2)
-                    const {body} = fetch.mock.calls[1][1]
-                    const expectedBody = JSON.stringify({'value1': 'No relevant traffic events'})
-                    expect(body).toBe(expectedBody)
-                    done()
-                }
-            })
-    })
-    test('non-GET methods should be rejected', (done) => {
-        trigger511(
-            { method: 'POST' },
-            {
-                sendStatus: (status) => {
-                    expect(status).toBe(405)
-                    done()
-                }
-            })
-    })
+    trigger511(
+      { method: 'GET' },
+      {
+        sendStatus: (status) => {
+          expect(status).toBe(200)
+          expect(fetch.mock.calls.length).toBe(2)
+          const { body } = fetch.mock.calls[1][1]
+          const expectedBody = JSON.stringify({ 'value1': 'No relevant traffic events' })
+          expect(body).toBe(expectedBody)
+          done()
+        }
+      })
+  })
+  test('non-GET methods should be rejected', (done) => {
+    trigger511(
+      { method: 'POST' },
+      {
+        sendStatus: (status) => {
+          expect(status).toBe(405)
+          done()
+        }
+      })
+  })
 })
